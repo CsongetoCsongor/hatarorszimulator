ee8dda4daa6db911c2f5a1c177dc515eef79ff8d
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2025-02-20 11:15:34 +0100

    shop, balance, előző kör div

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import  {generatePersonCarCombination}  from "./generatePersonCarCombination.js"
 import { quickTimeEvent } from "./quickTimeEvent.js";
 
 
-let player = new Player(0, "neutral", ["../player-arckifejezesek/neutral.png", "../player-arckifejezesek/angry.png"], "../fegyver-kepek-skinek/pistol-skin-4.jpg");
+let player = new Player(0, "neutral", ["../player-arckifejezesek/neutral.png", "../player-arckifejezesek/angry.png"], "../fegyver-kepek-skinek/ak-skin-1.png");
 
 console.log(generatePersonCarCombination());
 
